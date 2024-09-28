2a46e1d171b7d9b1a6123186401e1471e1413836
Ayush Agarwal <114360050+ayushagcode@users.noreply.github.com>
2024-09-29 02:04:56 +0530

    updated jobController & jobRouter

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -12,7 +12,8 @@ export const postJob = catchAsyncErrors(async(req,res,next)=>{
     qualifications, 
     introduction, 
     responsibilities,
-    offers, salary, 
+    offers, 
+    salary, 
     hiringMultipleCandidates, 
     personalWebsiteTitle, 
     personalWebsiteUrl,
@@ -66,3 +67,63 @@ export const postJob = catchAsyncErrors(async(req,res,next)=>{
         job,
     });
 });
+
+export const getAllJobs = catchAsyncErrors(async(req,res,next)=>{
+    const {city,niche,searchKeyword} = req.query;
+    const query = {};
+    if(city){
+        query.location = city;
+    }
+    if(niche){
+        query.jobNiche = niche;
+    }
+    if(searchKeyword){
+        query.$or = [
+            {title: {$regex:searchKeyword,$options: "i"}},
+            {companyName: {$regex:searchKeyword,$options: "i"}} ,
+            {introduction: {$regex:searchKeyword,$options: "i"}},
+        ];
+    }
+
+    const jobs = await Job.find(query);
+    res.status(200).json({
+        success:true,
+        jobs,
+        count:jobs.length
+    });
+})
+export const getMyJobs = catchAsyncErrors(async(req,res,next)=>{
+    const myJobs = await Job.find({ postedBy: req.user._id });
+    res.status(200).json({
+    success: true,
+    myJobs,
+  });
+})
+export const deleteJob = catchAsyncErrors(async(req,res,next)=>{
+    const { id } = req.params;
+    const job = await Job.findById(id);
+    if (!job) {
+        return next(new ErrorHandler("Oops! Job not found.", 404));
+    }
+    await job.deleteOne();
+    res.status(200).json({
+        success: true,
+        message: "Job Deleted!",
+    });
+})
+
+export const getASingleJob = catchAsyncErrors(async(req,res,next)=>{
+    const {id} = req.params;
+    try {
+        const job = await Job.findById(id);
+        if (!job){
+        return next(new ErrorHandler("Job not found.", 404));
+        }
+        res.status(200).json({
+        success: true,
+        job,
+        });
+    }catch (error){
+        return next(new ErrorHandler(`Invalid ID / CastError`, 404));
+    }
+})
--- backend/router/jobRouter.js
+++ backend/router/jobRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
-import { postJob } from "../controllers/jobController.js";
+import { postJob,getASingleJob,deleteJob,getAllJobs,getMyJobs} from "../controllers/jobController.js";
 
 const router = express.Router();
 // job can be posted by employer
 router.post("/post",isAuthenticated,isAuthorized("Employer"),postJob);
+router.get("/getall",getAllJobs);
+router.get("/getmyjobs", isAuthenticated, isAuthorized("Employer"),getMyJobs);
+router.delete("/delete/:id", isAuthenticated, isAuthorized("Employer"),deleteJob);
+router.get("/get/:id", isAuthenticated, getASingleJob);
+// router.put("/update/:id", isAuthenticated, updateJob);
 
- 
 export default router;
  
